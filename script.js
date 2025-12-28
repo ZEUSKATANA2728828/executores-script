e9ad154c6607a7060a8357993d9ec0ae29d06333
zeus-executores <teo92943@gmail.com>
2025-12-28 13:41:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+const aviso = document.getElementById("aviso");
+const countEl = document.getElementById("count");
+const toast = document.getElementById("toast");
+
+if (localStorage.getItem("aceito")) {
+  aviso.style.display = "none";
+}
+
+let downloads = localStorage.getItem("downloads") || 0;
+countEl.textContent = downloads;
+
 function aceitar() {
-  document.getElementById("aviso").style.display = "none";
+  localStorage.setItem("aceito", "true");
+  aviso.style.display = "none";
 }
 
 function recusar() {
   window.location.href = "https://www.google.com";
+}
+
+function baixar() {
+  downloads++;
+  localStorage.setItem("downloads", downloads);
+  countEl.textContent = downloads;
+
+  toast.style.display = "block";
+  setTimeout(() => toast.style.display = "none", 2000);
+
+  setTimeout(() => {
+    window.open("https://fgd8.short.gy/DELTA", "_blank");
+  }, 800);
 }
